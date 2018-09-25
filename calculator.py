78fa581e0d7fc433abf2b5a24b7007c9a6ea8319
Chi Cheng Min <chicm.dev@gmail.com>
2018-09-26 01:13:50 +0800

    updates

--- calculator.py
+++ calculator.py
@@ -50,9 +50,40 @@ def calculate_mean():
     img_sum = torch.sum(img_sum, 0)
     print(img_sum.size())
     img_sum = torch.sum(torch.sum(img_sum, 1), 1)
-    print(img_sum / (1000*32*224*224))
     
+    mean = img_sum / (1000*32*224*224)
+    print(mean)
+    return mean
+    
+
+def calculate_std(rgb_mean):
+    rgb_mean = torch.unsqueeze(rgb_mean, -1)
+    rgb_mean = torch.unsqueeze(rgb_mean, -1)
+    rgb_mean = torch.unsqueeze(rgb_mean, 0)
+
+    bg = time.time()
+
+    dset = TrainDataset(settings.TRAIN_IMG_DIR)
+    print(len(dset))
+    dloader = data.DataLoader(dset, batch_size=32, shuffle=False, num_workers=4, drop_last=True)
+    std_sum = None
+    for i, img in enumerate(dloader):
+        img = img.cuda()
+        if i % 100 == 0:
+            print(i, time.time() - bg)
+        if std_sum is None:
+            std_sum = (img - rgb_mean) * (img - rgb_mean)
+        else:
+            std_sum += (img - rgb_mean) * (img - rgb_mean)
+        if i == 999:
+            break
+    print(std_sum.size()) 
+    std_sum = torch.sum(std_sum, 0)
+    print(std_sum.size()) 
+    std_sum = torch.sum(torch.sum(std_sum, 1), 1)
+    print(torch.sqrt(std_sum / (1000*32*224*224-1)))
 
 
 if __name__ == '__main__':
-    calculate_mean()
+    rgb_mean = calculate_mean()
+    calculate_std(rgb_mean)
