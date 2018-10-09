3a4e77ce6144797896fcd6c7916555a3734859ba
Chi Cheng Min <chicm.dev@gmail.com>
2018-10-10 01:04:09 +0800

    updates

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import pandas as pd
 import settings
 from models import create_model
 from loader import get_test_loader, get_tuning_loader, get_train_val_loaders
-from metrics import find_threshold, f2_score
+from metrics import find_threshold, f2_score, find_fix_threshold, f2_scores
 from utils import get_classes
 
 def create_prediction_model(args):
@@ -40,9 +40,13 @@ def find_best_thresholds(model, val_loader):
                 outputs = torch.cat([outputs, output])    
 
     best_th = find_threshold(outputs, targets)
+    fix_th = find_fix_threshold(outputs, targets)
     #best_th[0] = 0.5
     optimized_score = f2_score(targets, torch.sigmoid(outputs), threshold=torch.Tensor(best_th).cuda())
-    return best_th, optimized_score
+    fix_score = f2_score(targets, torch.sigmoid(outputs), fix_th)
+
+    print(f2_scores(outputs, targets))
+    return best_th, optimized_score, fix_th, fix_score
 
 def get_label_names(row, classes):
     assert len(row) == len(classes)
@@ -51,7 +55,7 @@ def get_label_names(row, classes):
     assert len(tmp) == len(label_names)
     return ' '.join(label_names)
 
-def model_predict(args, model):
+def model_predict(args, model, check):
     model.eval()
     test_loader = get_test_loader(args, batch_size=args.batch_size, dev_mode=False)
 
@@ -67,33 +71,41 @@ def model_predict(args, model):
                 outputs = torch.cat([outputs, output])
             print('{}/{}'.format(args.batch_size*(i+1), test_loader.num), end='\r')
 
-            #if i == 0:
-            #    break
+            if check and i == 0:
+                break
  
     return outputs
 
 def predict(args):
     model = create_prediction_model(args)
-    _, val_loader = get_train_val_loaders(args, batch_size=args.batch_size)
-    #val_loader = get_tuning_loader(args, batch_size=args.batch_size)
+    #_, val_loader = get_train_val_loaders(args, batch_size=args.batch_size)
+    val_loader = get_tuning_loader(args, batch_size=args.batch_size)
 
-    thresholds, tuning_f2 = find_best_thresholds(model, val_loader)
-    print(thresholds)
+    opt_thresholds, tuning_f2, fix_th, fix_score = find_best_thresholds(model, val_loader)
+    print(opt_thresholds)
     print('optimized tuning f2:', tuning_f2)
+    print('fixed th:', fix_th)
+    print('fixed score:', fix_score)
+    
+
+    opt_thresholds = torch.Tensor(opt_thresholds).cuda()
+    pred_th = opt_thresholds #0.04 #fix_th
 
     if args.val:
         return
 
-    outputs = model_predict(args, model)
+    outputs = model_predict(args, model, args.check)
 
     classes, _ = get_classes(args.cls_type, args.start_index, args.end_index)
 
     label_names = []
-    pred = (outputs > torch.Tensor(thresholds).cuda()).byte().cpu().numpy()
+    pred = (outputs > pred_th).byte().cpu().numpy()
     for row in pred:
         label_names.append(get_label_names(row, classes))
 
-    #print(label_names)
+    if args.check:
+        print(label_names)
+        return
 
     create_submission(label_names, args.out_file)
 
@@ -102,34 +114,17 @@ def create_submission(predictions, outfile):
     meta['labels'] = predictions
     meta.to_csv(outfile, index=False)
 
-'''
-def ensemble():
-    outputs = []
-    for layer in [18, 34, 50]:
-        model = create_model('resnet', layer, pretrained=True)
-        output = model_predict(model)
-        outputs.append(output)
-    mean_output = torch.mean(torch.stack(outputs), 0)
-
-    label_names = []
-    pred = (mean_output > threshold).byte().cpu().numpy()
-    for row in pred:
-        label_names.append(get_label_names(row))
-
-    #print(label_names)
-    create_submission(label_names)
-'''
 
 def merge_dfs():
-    df1 = pd.read_csv('res34_optim_threshold.csv', index_col='image_id')
+    df1 = pd.read_csv('sub_tuning_0_100.csv', index_col='image_id', na_filter=False)
     df1.columns = ['label1']
-    df2 = pd.read_csv('sub_res34_50_200.csv', index_col='image_id')
+    df2 = pd.read_csv('sub_res34_100_200_th_020.csv', index_col='image_id', na_filter=False)
     df2.columns = ['label2']
     df3 = df1.join(df2)
     df3['labels'] = df3.label1.astype(str).str.cat(df3.label2.astype(str), sep=' ')
 
     print(df3.head())
-    df3.to_csv('merged_0_200_2.csv', columns=['labels'])
+    df3.to_csv('merged_tuning_0_200_2.csv', columns=['labels'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Inclusive')
@@ -138,11 +133,12 @@ if __name__ == '__main__':
     parser.add_argument('--layers', default=34, type=int, help='batch size')
     parser.add_argument('--scratch',action='store_true', help='train from scratch')
     parser.add_argument('--val',action='store_true', help='val only')
+    parser.add_argument('--check',action='store_true', help='check only')
     parser.add_argument('--cls_type', choices=['trainable', 'tuning'], type=str, required=True, help='class type')
     parser.add_argument('--start_index', type=int, required=True, help='start index of classes')
     parser.add_argument('--end_index', type=int, required=True, help='end index of classes')
-    parser.add_argument('--out_file', default='sub_res34_50_200.csv', type=str, help='submission file name')
+    parser.add_argument('--out_file', default='sub_res34_0_100_optimize_tuning.csv', type=str, help='submission file name')
     args = parser.parse_args()
 
-    #predict(args)
-    merge_dfs()
+    predict(args)
+    #merge_dfs()
