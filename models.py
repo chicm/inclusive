d6a128136249e20962ceab48497969d4ecef8816
Cheng Min Chi <idv2005@gmail.com>
2018-11-05 15:54:57 +0800

    updates

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from net.resnet import resnet18, resnet34, resnet50, resnet101, resnet152
-from net.senet import se_resnext50_32x4d, se_resnet50, senet154, se_resnet152
+from net.senet import se_resnext50_32x4d, se_resnet50, senet154, se_resnet152, se_resnext101_32x4d, se_resnet101
 from net.densenet import densenet121, densenet161, densenet169, densenet201
 import settings
 
@@ -14,7 +14,7 @@ class InclusiveNet(nn.Module):
     def __init__(self, backbone_name, num_classes=7172, pretrained=True):
         super(InclusiveNet, self).__init__()
         print('num_classes:', num_classes)
-        if backbone_name in ['se_resnext50_32x4d', 'se_resnet50', 'senet154', 'se_resnet152']:
+        if backbone_name in ['se_resnext50_32x4d', 'se_resnext101_32x4d', 'se_resnet101', 'se_resnet50', 'senet154', 'se_resnet152']:
             self.backbone = eval(backbone_name)()
         elif backbone_name in ['resnet34', 'resnet18', 'resnet50', 'resnet101', 'resnet152', 'densenet121', 'densenet161', 'densenet169', 'densenet201']:
             self.backbone = eval(backbone_name)(pretrained=pretrained)
